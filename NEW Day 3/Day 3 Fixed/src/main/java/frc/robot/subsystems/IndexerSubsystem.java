918dfc2faf03c67d2ef858d7111c85d05892b5ae
redt1979 <advik360@gmail.com>
2020-07-30 12:44:34 -0400

    Create IndexerSubsystem.java

---
+++ NEW Day 3/Day 3 Fixed/src/main/java/frc/robot/subsystems/IndexerSubsystem.java
@@ -0,0 +1,33 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class IndexerSubsystem extends SubsystemBase {
+  /**
+   * Creates a new IndexerSubsystem.
+   */
+  public IndexerSubsystem() {
+    private CANSparkMax spark1 = new CANSparkMax(0, CANSparkMaxLowLevel.MotorType.kBrushless);
+    private CANSparkMax spark2 = new CANSparkMax(1, CANSparkMaxLowLevel.MotorType.kBrushless);
+    private TalonSRX talon1 = new TalonSRX(2);
+  }
+
+  public storeBallUp() {
+    
+  }
+  public shootBall() {
+
+  }
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+}
