f3e3774bf61f407d0975437175f191fc358fe17e
leaderbeater <68249652+leaderbeater@users.noreply.github.com>
2020-07-30 13:41:45 -0400

    I hope the button configuration is understanderable
    I still need to improve in coding buttons in robot container. So help would be appreciated if I did it wrong.

---
+++ Day3/Kevin May/Leaderheater/src/main/java/frc/robot/subsystems/Indexer.java
@@ -0,0 +1,44 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+
+public class ExampleSubsystem extends SubsystemBase {
+  /**
+   * Creates a new ExampleSubsystem.
+   */
+  private final CANSparkMax rightMotor;
+  private final CANSparkMax leftMotor;
+  private final TalonSRX centralMotor; 
+  public Indexer() {
+
+  CANSparkMax rightMotor = new CANSparkMax(constants.RIGHT_MOTOR, CANSparkMaxLowLevel.MotorType.kBrushless);
+  CANSparkMax leftMotor = new CANSparkMax(constants.LEFT_MOTOR, CANSparkMaxLowLevel.MotorType.kBrushless);
+  TalonSRX centralMotor = new TalonSRX(2);
+  }
+  public void moveUp() {
+    rightMotor.set(1);
+
+  }
+  public void moveDown() {
+    leftMotor.set(-1);
+  }
+  public void runSet() {
+    centralMotor.set(1);
+  }
+  @Override
+  public void periodic() {
+
+  }
+}
